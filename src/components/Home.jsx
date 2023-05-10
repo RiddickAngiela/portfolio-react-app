7a904c9832dd46965039e399a31b0c2a93dadccb
RiddickAngiela <angielariddick@gmail.com>
2023-05-10 12:22:27 +0300

    Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,20 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
   return (
-    <div>
-
+    <div name='home' className='bg-[#0a192f] w-full h-screen'>
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <p className='text-pink-600'>Hi, My name is</p>
+                <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Riddick Washington Angiela</h1>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer & Innovation Technologist</h2>
+                <p className='text-[#8892b0] py-4 max-w-[700px]'>Im a full-stack developer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, Im focused on
+          building responsive full-stack web applications, using React and Ruby</p>
+          <div>
+            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work 
+            <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3'/></span></button>
+          </div>
+        </div>
     </div>
   )
 }
